f51c3bf2ba443826f624e50d32ca68bc94234441
KneeSock <2503648224@qq.com>
2023-09-25 21:13:36 +0800

    缩放居中

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from "./router"
 //引入Element-plus
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 //axios
@@ -15,4 +16,7 @@ app.config.globalProperties.$axios = axios
 app.use(router)
 //Element-plus
 app.use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.mount('#app')
