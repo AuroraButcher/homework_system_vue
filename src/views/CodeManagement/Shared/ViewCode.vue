be3facb312599dfc26c932c360bf9abe18a0c6bc
Aurora <2411467790@qq.com>
2023-12-06 20:05:01 +0800

    结构优化

--- src/views/CodeManagement/Shared/ViewCode.vue
+++ src/views/CodeManagement/Shared/ViewCode.vue
@@ -6,7 +6,6 @@
     </template>
     <el-table :data="tableData" border style="width:100%;margin-top: 10px" :row-class-name="rowClassName" :Key="key">
       <el-table-column label="序号" type="index" width="60px"></el-table-column>
-      <!-- <el-table-column label="课程编号" prop="id" width="100px" v-if="false"></el-table-column> -->
       <el-table-column label="题目名称" prop="name" width="200px">
         <template #default="scope">
           <el-link link @click="showDetailInfo(scope)">{{ scope.row.title }}</el-link>
@@ -38,137 +37,46 @@ import api from "../../../api/index.js";
 import PageHeader from "../../Base/PageHeader.vue";
 
 export default {
-    name: "checkProgram",
-    components: { PageHeader },
-    data() {
-        return {
-            head: '题目列表',
-            courseName: '',
-            key: 1,
-            tableData: [
-                {
-                    "id": null,
-                    "title": null,
-                    "start": null,
-                    "end": null,
-                }
-            ],
-            page: {
-                teacherName: '',
-                teacherNumber: '',
-                studentNumber: '',
-                currentPage: 1,
-                pageSize: 10,
-                total: null,
-            },
+  name: "checkProgram",
+  components: { PageHeader },
+  data() {
+    return {
+      head: '题目列表',
+      courseName: '',
+      key: 1,
+      tableData: [
+        {
+            "id": null,
+            "title": null,
+            "start": null,
+            "end": null,
         }
-    },
-    // 展示课程信息
-    created() {
-        if (this.role === 'administrator') {
-            api.showCourse(this.page).then(response => {
-                if (response.data.code === 20000) {
-                    this.page.total = response.data.data.classInfo.total;
-                    this.tableData = response.data.data.classInfo.records;
-                } else {
-                    console.log("查看课程列表失败");
-                }
-            })
-        } else if (this.role === 'student') {
-            this.page.studentNumber = Cookies.get('number');
-            api.getCodeListForStudent(this.page).then(respose => {
-                    if (respose.data.code === 20000) {
-                        this.page.total = respose.data.data.list.total;
-                        this.tableData = respose.data.data.list.records;
-                    } else {
-                        console.log("查看课程列表失败");
-                    }
-                })
-        } else {
-            this.page.teacherNumber = Cookies.get('number');
-            api.teacherShowCourse(this.page).then(response => {
-                if (response.data.code === 20000) {
-                    this.page.total = response.data.data.classInfo.total;
-                    this.tableData = response.data.data.classInfo.records;
-                } else {
-                    console.log("查看课程列表失败");
-                }
-            })
-        }
-    },
-    methods: {
-        //搜索课程
-        search(courseName) {
-            this.page.courseName = courseName;
-            if (this.role === 'administrator') {
-                api.showCourse(this.page).then(response => {
-                    if (response.data.code === 20000) {
-                        this.page.total = response.data.data.classInfo.total;
-                        this.tableData = response.data.data.classInfo.records;
-                    } else {
-                        console.log("查看课程列表失败");
-                    }
-                })
-            } else if (this.role === 'student') {
-
-                  this.page.studentNumber = Cookies.get('number');
-                  api.studentShowCourse(this.page).then(response => {
-                    if (response.data.code === 20000) {
-                      this.page.total = response.data.data.classInfo.total;
-                      this.tableData = response.data.data.classInfo.records;
-                    } else {
-                      console.log("查看课程列表失败");
-                    }
-                  })
-            } else {
-                this.page.teacherNumber = Cookies.get('number');
-                api.teacherShowCourse(this.page).then(response => {
-                    if (response.data.code === 20000) {
-                        this.page.total = response.data.data.classInfo.total;
-                        this.tableData = response.data.data.classInfo.records;
-                    } else {
-                        console.log("查看课程列表失败");
-                    }
-                })
-            }
-        },
-        //展示详细信息
-        showDetailInfo(scope) {
-            this.$store.commit('setCodeId', scope.row.id);
-            this.$router.push('/viewCode');
-        },
-        showSubmissionList(scope) {
-            this.$store.commit('setCodeId', scope.row.id);
-            this.$router.push("/submissionList")
-        },
-        // 处理页数改变
-        handlePageChange() {
-            api.getCodeListForStudent(this.page).then(response => {
-                if (response.data.code === 20000) {
-                    console.log(this.page.currentPage)
-                    console.log(this.page.pageSize)
-                    console.log(response.data.data.list.records)
-                    this.page.total = response.data.data.list.total;
-                    this.tableData = response.data.data.list.records;
-                } else {
-                    ElMessage.error(response.data.message);
-                }
-            })
-        },
-        //获取当前的行数
-        rowClassName({ row, rowIndex }) {
-            //把每一行的索引放进row
-            row.index = rowIndex;
-        },
-        // 弹出框
-        addStudent(scope) {
-            this.dialogFileVisible = true;
-            this.$store.commit('setCourseNumber', scope.row.id);
-        },
-    },
-    computed: {
-        ...mapState(['role', 'courseNumber'])
+      ],
+      page: {
+        teacherName: '',
+        teacherNumber: '',
+        studentNumber: '',
+        currentPage: 1,
+        pageSize: 10,
+        total: null,
+      },
     }
+  },
+  // 展示代码作业信息
+  created() {
+
+  },
+  methods: {
+    // 处理页数改变
+    handlePageChange(){
+
+    },
+    // 展示代码作业信息
+
+  },
+  computed: {
+    ...mapState(['role', 'courseNumber'])
+  }
 }
 </script>
   
